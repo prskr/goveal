667bc0e2cb3cfa2e76eef917e729f5754f0c490a
Peter Kurfer <peter.kurfer@googlemail.com>
2019-05-16 12:18:28 +0200

    fix: added missing fonts to assets

--- download_revealjs.sh
+++ download_revealjs.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 mkdir -p ./assets/reveal
-curl -sL "https://github.com/hakimel/reveal.js/archive/${1:-3.8.0}.tar.gz" | tar -xvz --strip-components=1 -C ./assets/reveal --wildcards *.js --wildcards *.css --wildcards *.html --exclude test --exclude gruntfile.js
+curl -sL "https://github.com/hakimel/reveal.js/archive/${1:-3.8.0}.tar.gz" | tar -xvz --strip-components=1 -C ./assets/reveal \
+                                                                                                                --wildcards *.js \
+                                                                                                                --wildcards *.css \
+                                                                                                                --wildcards *.html \
+                                                                                                                --wildcards *.woff \
+                                                                                                                --wildcards *.ttf \
+                                                                                                                --exclude test \
+                                                                                                                --exclude gruntfile.js
 mkdir -p ./assets/reveal/plugin/
 git clone https://github.com/denehyg/reveal.js-menu.git ./assets/reveal/plugin/menu
 
